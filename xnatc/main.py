f8e97fb0c5480203a960075d0438e4e673aa4473
Martin Craig <martin.craig@eng.ox.ac.uk>
2021-09-13 11:19:59 +0100

    Add prompt for username/password if not in .netrc file

--- xnatc/main.py
+++ xnatc/main.py
@@ -2,7 +2,10 @@
 xnatc - Command line interface to XNAT
 """
 import argparse
+import getpass
+import netrc
 import sys
+from urllib.parse import urlparse
 
 import xnat
 
@@ -22,6 +25,23 @@ def label(obj):
     else:
         return obj.id
 
+def get_auth(args):
+    if args.user and args.password:
+        return
+    if '//' not in args.xnat:
+        print("WARNING: Xnat host did not have HTTP or HTTPS specified - assuming HTTPS")
+        args.xnat = 'https://' + args.xnat
+    url = urlparse(args.xnat)
+    auth_data = netrc.netrc()
+    if url.hostname in auth_data.hosts:
+        args.user, _account, args.password = auth_data.authenticators(url.hostname)
+    else:
+        print("WARNING: No authentication information found in $HOME/.netrc")
+        args.user = input("Username: ").strip()
+        args.password = getpass.getpass()
+        print("  Note: You can add your credentials to $HOME/.netrc for automatic login")
+        print("        See https://xnat.readthedocs.io/en/latest/static/tutorial.html#credentials")
+
 def main():
     parser = argparse.ArgumentParser(description='Command line interface to XNAT')
     parser.add_argument('--xnat', default='https://xnatpriv.nottingham.ac.uk/', help='xnat host URL')
@@ -30,10 +50,14 @@ def main():
     parser.add_argument('--experiment', help='Experiment ID')
     parser.add_argument('--scan', help='Scan ID')
     parser.add_argument('--download', help='Download data to named directory')
-    parser.add_argument('--recurse', action="store_true", help='List contents of selected object')
+    parser.add_argument('--user', help='XNAT user name. If not specified will use credentials from $HOME.netrc or prompt for username')
+    parser.add_argument('--password', help='XNAT password. If not specified will use credentials from $HOME.netrc or prompt for password')
+    parser.add_argument('--recurse', action="store_true", help='Recursively list contents of selected object')
     args = parser.parse_args()
 
-    with xnat.connect(args.xnat) as connection:
+    get_auth(args)
+
+    with xnat.connect(args.xnat, user=args.user, password=args.password) as connection:
         connection.xnat_url = args.xnat
         if args.download and not (args.project and args.subject and args.experiment):
             check = input("WARNING: download requested for multiple experiment - are you sure? ")
@@ -41,6 +65,8 @@ def main():
                 sys.exit(0)
 
         process(connection, args, 0)
+        if args.download:
+            print("Data downloaded to %s", args.download)
 
 def process(obj, args, hierarchy_idx, indent="", recurse=True):
     obj_type = HIERARCHY[hierarchy_idx]
